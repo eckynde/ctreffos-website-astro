5443778f970360683b31df4ea16d44734a2dd31a
Kolja Lampe <razzeee@gmail.com>
2025-02-16 18:51:47 +0100

    Set site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ctreffos.github.io",
+  base: "ctreffos-website-astro",
   integrations: [
     react(),
     tailwind({
