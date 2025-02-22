c1328d26618bc166d14b80de843a4dd2f6faee22
Razze <razzeee@gmail.com>
2025-02-22 22:19:55 +0100

    Improve calendar

--- src/pages/en/calendar.ics.ts
+++ src/pages/en/calendar.ics.ts
@@ -1,7 +1,11 @@
 import { getCollection } from "astro:content";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
-import ical, { ICalCalendarMethod } from "ical-generator";
+import ical, {
+  ICalCalendarMethod,
+  ICalEventRepeatingFreq,
+  type ICalDescription,
+} from "ical-generator";
 const parser = new MarkdownIt();
 
 const lang = "en";
@@ -16,17 +20,41 @@ export async function GET(context: { site: string }) {
   // A method is required for outlook to display event as an invitation
   calendar.method(ICalCalendarMethod.REQUEST);
 
+  calendar.createEvent({
+    start: new Date(2024, 8, 6, 19),
+    end: new Date(2024, 8, 6, 23),
+    summary: "Chaostreff",
+    description: {
+      plain: "Das woechentliche Treffen jeden Donnerstag um 19:00 Uhr statt.",
+    } as ICalDescription,
+    location: "Uni AStA Osnabrueck, Alte Muenze 12, 49074 Osnabrueck",
+    repeating: {
+      freq: ICalEventRepeatingFreq.WEEKLY,
+    },
+    timezone: "Europe/Berlin",
+  });
+
   events.forEach((event) => {
     calendar.createEvent({
       start: event.data.startDate,
       end: event.data.endDate,
       summary: event.data.title,
-      description:
-        event.body?.length > 0
-          ? sanitizeHtml(parser.render(event.body))
-          : undefined,
+      description: {
+        plain:
+          event.body?.trim().length > 0
+            ? sanitizeHtml(parser.render(event.body), {
+                allowedTags: [],
+                allowedAttributes: {},
+              })
+            : "",
+        html:
+          event.body?.trim().length > 0
+            ? sanitizeHtml(parser.render(event.body))
+            : "",
+      } as ICalDescription,
       location: event.data.location,
       url: `${context.site}about#${event.slug}`,
+      timezone: "Europe/Berlin",
     });
   });
 
